89bcfa5bde314b512a90b430e4a3d0e2ef830a8f
xiayiye5 <13343401268@qq.com>
2019-12-13 18:39:45 +0800

    增加事件分发demo

---
+++ app/src/main/java/com/xiayiye/honorfirst/custom/CustomView.java
@@ -0,0 +1,47 @@
+package com.xiayiye.honorfirst.custom;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+/**
+ * @author xiayiye
+ */
+public class CustomView extends View {
+    public CustomView(Context context) {
+        this(context, null);
+    }
+
+    public CustomView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CustomView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        Log.e("打印CustomView", "dispatchTouchEvent");
+        boolean b = super.dispatchTouchEvent(event);
+        Log.e("打印CustomView", "dispatchTouchEvent:" + b);
+        return b;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.e("打印CustomView", "onTouchEvent");
+        Toast.makeText(getContext(), "点击了里面的CustomView", Toast.LENGTH_SHORT).show();
+        boolean b = super.onTouchEvent(event);
+        Log.e("打印CustomView", "onTouchEvent:" + b);
+        return true;
+    }
+}
---
+++ app/src/main/java/com/xiayiye/honorfirst/custom/CustomViewGroup.java
@@ -0,0 +1,84 @@
+package com.xiayiye.honorfirst.custom;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+/**
+ * @author xiayiye
+ */
+public class CustomViewGroup extends LinearLayout {
+    public CustomViewGroup(Context context) {
+        this(context, null);
+    }
+
+    public CustomViewGroup(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CustomViewGroup(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+//        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        Paint paint = new Paint();
+        paint.setColor(Color.RED);
+        paint.setTypeface(Typeface.DEFAULT_BOLD);
+        paint.setAntiAlias(true);
+        paint.setTextSize(24);
+    }
+
+   /* @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+    }*/
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        Log.e("打印CustomViewGroup", "dispatchTouchEvent");
+        boolean b = super.dispatchTouchEvent(ev);
+        Log.e("打印CustomViewGroup", "dispatchTouchEvent:" + b);
+        return b;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        Log.e("打印CustomViewGroup", "onInterceptTouchEvent");
+        boolean b = super.onInterceptTouchEvent(ev);
+        Log.e("打印CustomViewGroup", "onInterceptTouchEvent:" + b);
+        return b;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.e("打印CustomViewGroup", "onTouchEvent");
+        Toast.makeText(getContext(), "点击了外层的ViewGroup", Toast.LENGTH_SHORT).show();
+        boolean b = super.onTouchEvent(event);
+        Log.e("打印CustomViewGroup", "onTouchEvent:" + b);
+        return b;
+    }
+}
