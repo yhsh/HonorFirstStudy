d44d56b58816e2b143ad102061ad800a53c91838
xiayiye5 <13343401268@qq.com>
2019-12-16 13:18:56 +0800

    增加Retrofit同步请求的方法

--- mvptest/src/main/java/com/example/mvptest/model/HomeModel.java
+++ mvptest/src/main/java/com/example/mvptest/model/HomeModel.java
@@ -1,5 +1,6 @@
 package com.example.mvptest.model;
 
+import android.os.Handler;
 import android.util.Log;
 
 import com.example.mvptest.api.RequestApi;
@@ -7,6 +8,8 @@ import com.example.mvptest.bean.HomeBean;
 import com.example.mvptest.contract.HomeContract;
 import com.example.mvptest.presenter.HomePresenter;
 import com.google.gson.Gson;
+
+import java.io.IOException;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -20,6 +23,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class HomeModel implements HomeContract.Model {
     private String str = "要显示的数据";
     private HomePresenter homePresenter;
+    Handler handler = new Handler();
 
     @Override
     public void setData(HomePresenter homePresenter) {
@@ -36,12 +40,48 @@ public class HomeModel implements HomeContract.Model {
         Retrofit retrofit = new Retrofit.Builder().baseUrl("https://wanandroid.com/").addConverterFactory(GsonConverterFactory.create()).build();
         RequestApi requestApi = retrofit.create(RequestApi.class);
         Call<HomeBean> homeBeanCall = requestApi.requestData(param);
+        //Retrofit同步方法
+        /*new Thread() {
+            @Override
+            public void run() {
+                String name1 = Thread.currentThread().getName();
+                Log.e("打印线程1", name1);
+                try {
+                    Response<HomeBean> execute = homeBeanCall.execute();
+                    HomeBean body = execute.body();
+                    String s = new Gson().toJson(body);
+                    Log.e("打印：", s + "=");
+                    if (body.getData().getDatas().size() > 0) {
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                String name2 = Thread.currentThread().getName();
+                                Log.e("打印线程2", name2);
+                                homePresenter.getSuccessData(body);
+                            }
+                        });
+                    } else {
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                homePresenter.getFailData("暂无数据");
+                            }
+                        });
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();*/
+
+        //Retrofit异步方法
         homeBeanCall.enqueue(new Callback<HomeBean>() {
             @Override
             public void onResponse(Call<HomeBean> call, Response<HomeBean> response) {
                 HomeBean body = response.body();
                 String s = new Gson().toJson(response.body());
                 Log.e("打印：", s + "=");
+                assert body != null;
                 if (body.getData().getDatas().size() > 0) {
                     homePresenter.getSuccessData(body);
                 } else {
@@ -51,10 +91,12 @@ public class HomeModel implements HomeContract.Model {
 
             @Override
             public void onFailure(Call<HomeBean> call, Throwable throwable) {
+                throwable.printStackTrace();
+                String message = throwable.getMessage();
+                String localizedMessage = throwable.getLocalizedMessage();
+                Log.e("打印异常：", message + "================\n" + localizedMessage);
                 homePresenter.getFailData("调用失败");
             }
         });
-        //模拟网络请求获取数据
-//        str += param;
     }
 }
